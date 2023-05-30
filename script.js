7950ab83fd39562880971367dfb70ed39e437ead
Taimeem Ansari <taimeemansari@Taimeems-MacBook-Pro.local>
2023-05-30 19:57:21 +0530

    button gradient style added

--- script.js
+++ script.js
@@ -32,11 +32,11 @@ window.addEventListener("DOMContentLoaded", () => {
   data.forEach((subject) => {
     let { category, color, icon, score } = subject;
     let clon = temp.content.cloneNode(true);
-    clon.querySelector("#result").classList.add(`bg-${color}200`);
-    clon.querySelector("#result img").setAttribute("src", icon);
-    clon.querySelector("#subjectName").classList.add(`text-${color}`);
-    clon.querySelector("#subjectName").textContent = category;
-    clon.querySelector("#score").textContent = score;
+    clon.querySelector(".result").classList.add(`bg-${color}200`);
+    clon.querySelector(".result img").setAttribute("src", icon);
+    clon.querySelector(".subjectName").classList.add(`text-${color}`);
+    clon.querySelector(".subjectName").textContent = category;
+    clon.querySelector(".score").textContent = score;
     summaryResults.appendChild(clon);
   });
 });
